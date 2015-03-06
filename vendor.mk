c7a59ab27a63add26b6aae657496e2405bd573ff
nobe1976 <charlesmc76@gmail.com>
2015-03-06 08:33:32 -0500

    Set Background apps defaults for all builds.

--- vendor.mk
+++ vendor.mk
@@ -20,6 +20,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
+# Background app control
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.bg_apps_limit=10
+
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
