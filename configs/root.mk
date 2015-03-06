d17e4b753889ef48829fda8532b72a2df0754ee1
nobe1976 <charlesmc76@gmail.com>
2015-03-06 16:15:01 -0500

    Add SuperSU to AOD 2/2
    Source changed
    
    android_build
    android_vendor_aod

---
+++ configs/root.mk
@@ -0,0 +1,5 @@
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/aod/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/aod/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
