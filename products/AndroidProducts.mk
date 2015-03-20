a07357c548df2c26aa83c687942f490e5110f646
nobe1976 <charlesmc76@gmail.com>
2015-03-20 15:43:14 -0400

    Forgot Sammy...

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,4 +3,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/icarus_xt925.mk \
     $(LOCAL_DIR)/icarus_xt926.mk \
     $(LOCAL_DIR)/icarus_vs980.mk \
+    $(LOCAL_DIR)/icarus_jfltevzw.mk \
     $(LOCAL_DIR)/icarus_shamu.mk
